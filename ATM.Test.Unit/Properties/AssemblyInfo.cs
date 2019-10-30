1403e9ea1fda8b16b230a75df00275ba7fc25588
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-10-30 12:58:27 +0100

    Fixed project folders and types

--- ATM.Test.Unit/Properties/AssemblyInfo.cs
+++ ATM.Test.Unit/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0efaf17b-0c9c-42b8-9f48-fb040349b5d6")]
+[assembly: Guid("96f7ed92-485a-4348-8d11-1068aedf0144")]
 
 // Version information for an assembly consists of the following four values:
 //
